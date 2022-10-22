ccbcb958bf89539cbae38d9e939891eee3ab2582
Gegardus <stgegard@gmail.com>
2022-10-22 15:47:42 +0400

    add user-friend input and validation

--- main.rb
+++ main.rb
@@ -9,7 +9,7 @@ players = [
 game = Game.new(players)
 
 puts "Welcome to Tic Tac Toe!"
-puts "Player's one turn."
+puts "It's player's one turn."
 puts game.display_board
 puts  'Player one, pick a move:'
 p game.play_turn(players[0])
--- player.rb
+++ player.rb
@@ -1,4 +1,5 @@
 class Player 
+    ROW_MAP = (:A..:C).zip(0..2).to_h
     attr_reader :marker
 
     def initialize(name, marker)
@@ -11,11 +12,40 @@ class Player
       [get_row, get_col]  
     end
 
-    def get_row       
-      gets.to_i
+    def get_row   
+      loop do
+        prompt_choice('row')
+        input = gets
+        choice = translate_row(input)
+        return choice if in_bounds?(choice) 
+        puts 'Invalid row.'
+      end
     end
 
-    def get_col     
-     gets.to_i
+    def get_col
+      loop do
+        prompt_choice('column')
+        input = gets
+        choice = translate_col(input)
+        return choice if in_bounds?(choice) 
+        puts 'Invalid column.'
+      end  
+    end
+
+    def translate_row(input) 
+      ROW_MAP[input[0].upcase.to_sym] 
+    end
+
+    def translate_col(input) 
+      input.to_i - 1      
+    end
+
+    def prompt_choice(string) 
+      puts "Pick a #{string}"
+      print "> " 
+    end
+
+    def in_bounds?(choice) 
+      (0..2).include?(choice)
     end
 end 
